b4c86b4fa17adebd6ca4f9e5ec77c8ffa4d21c41
DiogoWernek <diogowernek86@gmail.com>
2025-03-03 13:00:25 -0300

    Api finalizada

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,5 +1,7 @@
+import { updateStatus } from "../controllers/tickets/updateStatus.js";
 import { create } from "../controllers/tickets/create.js";
 import { update } from "../controllers/tickets/update.js";
+import { remove } from "../controllers/tickets/remove.js";
 import { index } from "../controllers/tickets/index.js";
 
 export const tickets = [
@@ -19,5 +21,17 @@ export const tickets = [
     method: "PUT",
     path: "/tickets/:id",
     controller: update,
+  },
+
+  {
+    method: "PATCH",
+    path: "/tickets/:id/close",
+    controller: updateStatus,
+  },
+
+  {
+    method: "DELETE",
+    path: "/tickets/:id",
+    controller: remove,
   },
 ]
